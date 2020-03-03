aecc75364ca9d69ed28d42bd009fadef023341c1
Mark Magyarodi <ribizli@gmail.com>
2020-03-03 23:38:03 +0100

    added setTouched for Group and Array controls, too

--- control.d.ts
+++ control.d.ts
@@ -13,16 +13,18 @@ export interface $ControlState {
 declare type ControlState<T = any> = T extends (infer K)[] ? Array<ControlState<K> & $ControlState> : T extends ControlTypes ? $ControlState : T extends GroupValue<T> ? {
     [K in keyof T]: ControlState<T[K]> & $ControlState;
 } : $ControlState;
-export interface ControlBase<T = any> {
-    value: Writable<T>;
-    state: Readable<ControlState<T>>;
-    getControl(path: string): ControlBase;
-    reset(value?: T): void;
+export declare abstract class ControlBase<T = any> {
+    protected validators: ValidatorFn<T>[];
+    constructor(validators: ValidatorFn<T>[]);
+    abstract value: Writable<T>;
+    abstract state: Readable<ControlState<T>>;
+    abstract getControl(path: string): ControlBase;
+    abstract reset(value?: T): void;
+    abstract setTouched(touched: boolean): void;
     setValidators(validators: ValidatorFn<T>[]): void;
 }
-export declare class Control<T = ControlTypes> implements ControlBase<T> {
+export declare class Control<T = ControlTypes> extends ControlBase<T> {
     private initial;
-    private validators;
     value: Writable<T>;
     private touched;
     state: Readable<ControlState<T>>;
@@ -30,39 +32,38 @@ export declare class Control<T = ControlTypes> implements ControlBase<T> {
     setTouched(touched: boolean): void;
     getControl(): never;
     reset(value?: T): void;
-    setValidators(validators: ValidatorFn<T>[]): void;
 }
 declare type Controls<T> = {
     [K in keyof T]: ControlBase<T[K]>;
 };
-export declare class ControlGroup<T> implements ControlBase<T> {
+export declare class ControlGroup<T> extends ControlBase<T> {
     private readonly controls;
-    private validators;
     private valueDerived;
     value: Writable<T>;
     state: Readable<ControlState<T>>;
     constructor(controls: Controls<T>, validators?: ValidatorFn<T>[]);
     private initControls;
     private setValue;
+    setTouched(touched: boolean): void;
     getControl(path: string): ControlBase<any>;
     reset(value?: T): void;
-    setValidators(validators: ValidatorFn<T>[]): void;
 }
-export declare class ControlArray<T> implements ControlBase<T[]> {
-    private readonly controls;
-    private validators;
+export declare class ControlArray<T> extends ControlBase<T[]> {
+    private readonly _controls;
     private controlStore;
     private valueDerived;
     value: Writable<T[]>;
     state: Readable<(ControlState<T> & $ControlState)[]>;
-    constructor(controls: ControlBase<T>[], validators?: ValidatorFn<T[]>[]);
+    constructor(_controls: ControlBase<T>[], validators?: ValidatorFn<T[]>[]);
     private setValue;
+    setTouched(touched: boolean): void;
     get size(): number;
+    get controls(): ControlBase<T>[];
     pushControl(control: ControlBase<T>): void;
     addControlAt(index: number, control: ControlBase<T>): void;
     removeControlAt(index: number): void;
     slice(start?: number, end?: number): void;
-    getControl(path: string): ControlBase<any>;
+    getControl(path: string): ControlBase<any> | ControlBase<T>;
     reset(value?: T[]): void;
     setValidators(validators: ValidatorFn<T[]>[]): void;
 }
